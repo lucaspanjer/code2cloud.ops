036589b3b3a989f45f8a1228f827e12bc9b99ead
Clint Morgan <clint.morgan@tasktop.com>
2012-02-23 18:47:53 -0800

    task 4111: Support deploying to Oracle Linux https://q.tasktop.com/alm/#projects/c2c/task/4111
    re-arranging sync script to make it generic
    move roles to subdir
    add roles for cluster node types

---
+++ solo/bootstrap/configureNode.rb
@@ -0,0 +1,2 @@
+#TODO
+# Java setup
---
+++ solo/sync-node.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+TOP_DIR="`pwd`/`dirname $0`/../"
+SCP="scp"
+SSH_HOST="vcloud@192.168.1.100"
+CHEF_DIR="/opt/code2cloud/chef"
+ENV="q-selfhost"
+
+if [ ! -d "${TOP_DIR}/roles/${ENV}" ]; then
+    echo "'${ENV}' environment does not exist"
+    exit 1
+fi
+
+echo "Using '${ENV}' environment..."
+
+$SCP ${TOP_DIR}/roles/${ENV}/*.rb $SSH_HOST:$CHEF_DIR/roles
+$SCP ${TOP_DIR}/roles/*.rb $SSH_HOST:$CHEF_DIR/roles
+
+CHEF_SERVER_DEPS="build-essential bluepill couchdb daemontools erlang gecode runit openssl ucspi-tcp xml zlib"
+COOKBOOKS="apt apache2 chef-server emacs git java mysql openssh postfix ssh_known_hosts sudo tomcat jpackage maven ntp openssl"
+
+echo "uploading opscode cookbooks..."
+cd $TOP_DIR/../opscode-cookbooks
+$SCP -r $COOKBOOKS $SSH_HOST:$CHEF_DIR/cookbooks
+
+echo "uploading cfc cookbooks..."
+cd $TOP_DIR/site-cookbooks/
+$SCP -r ./* $SSH_HOST:$CHEF_DIR/cookbooks
+
+echo "uploading solo config..."
+$SCP $TOP_DIR/solo/chef-solo.sh $TOP_DIR/solo/solo.rb $SSH_HOST:$CHEF_DIR/ 
+$SCP $TOP_DIR/solo/ol-roles.json $SSH_HOST:$CHEF_DIR/roles.json
