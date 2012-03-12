dfcc781964e0de66e96c6610624a065fef27c96d
Clint Morgan <clint.morgan@tasktop.com>
2012-03-12 12:53:58 -0700

    use the latest version of grails

--- site-cookbooks/grails/attributes/default.rb
+++ site-cookbooks/grails/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:grails][:root_dir] = "/usr/share/grails"
 default[:grails][:mirror] = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS"
-default[:grails][:version] = "1.3.7"
+default[:grails][:version] = "2.0.1"
 default[:grails][:subdir] = "grails-#{node[:grails][:version]}"
 default[:grails][:path] = "#{node[:grails][:root_dir]}/#{node[:grails][:subdir]}"
 default[:grails][:bin] = "#{node[:grails][:path]}/bin"
