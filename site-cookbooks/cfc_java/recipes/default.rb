7faf6cad25f523857cee9dda0586fb81bfe2dae4
Clint Morgan <clint.morgan@tasktop.com>
2012-04-25 14:56:36 -0700

    4236: sometimes oracleserver is reported as oracle https://q.tasktop.com/alm/#projects/c2c/task/4236

--- site-cookbooks/cfc_java/recipes/default.rb
+++ site-cookbooks/cfc_java/recipes/default.rb
@@ -1,4 +1,4 @@
-if platform?("redhat", "oracleserver")
+if platform?("redhat", "oracleserver", "oracle")
   unless File.exists?("/usr/bin/java")
     remote_file "/tmp/install-java.bin" do
       source "http://download.oracle.com/otn-pub/java/jdk/6u31-b04/jdk-6u31-linux-x64-rpm.bin"
