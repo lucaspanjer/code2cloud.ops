e2904422e3ca2d3f0810c4e1ea8f89e2062a08f0
Clint Morgan <clint.morgan@tasktop.com>
2012-05-29 16:47:52 -0700

    default to no email logging, fix issue with newer version of chef

--- site-cookbooks/cfc_server/attributes/default.rb
+++ site-cookbooks/cfc_server/attributes/default.rb
@@ -5,6 +5,7 @@ default[:cfc][:server][:build] = nil #artifacts are not deployed unless build# i
 default[:cfc][:server][:version] = "0.1.0-SNAPSHOT"
 default[:cfc][:server][:root_package] = "com.tasktop.c2c.server"
 default[:cfc][:server][:artifacts] = []
+default[:cfc][:artifacts][:base_url] = "fixme"
 default[:cfc][:user] = "vcloud"
 default[:cfc][:group] = "vcloud"
 default[:cfc][:user_home_prefix] = "/home/"
@@ -15,6 +16,6 @@ default[:cfc][:server][:backup][:directories] = []
 default[:cfc][:server][:backup][:key_name] = "Code2Cloud"
 default[:cfc][:server][:s3][:access_key_id] = ""
 default[:cfc][:server][:s3][:access_key_secret] = ""
-default[:cfc][:server][:email_log_errors_to_admin] = true
+default[:cfc][:server][:email_log_errors_to_admin] = false
 override[:tomcat][:java_options] = "-Xmx2048M -Xss192K -XX:MaxPermSize=512M -Dlog4j.configuration=file://#{node.cfc.server.opt}/etc/log4j.xml"
 
