ff6f775491c33365194a1f0dc4f8320f8845d097
Clint Morgan <clint.morgan@tasktop.com>
2012-07-04 12:37:07 -0700

    Remove broken script to install java on oracle, just insist that it gets manually installed.

--- site-cookbooks/cfc_java/recipes/default.rb
+++ site-cookbooks/cfc_java/recipes/default.rb
@@ -1,11 +1,7 @@
 if platform?("redhat", "oracleserver", "oracle")
+  # FIXME this will not work anymore, need to agree to license agreement
   unless File.exists?("/usr/bin/java")
-    remote_file "/tmp/install-java.bin" do
-      source "http://download.oracle.com/otn-pub/java/jdk/6u31-b04/jdk-6u31-linux-x64-rpm.bin"
-      mode "0777"
-    end
-    execute "/tmp/install-java.bin" do
-    end
+    raise "Must manually install from: http://www.oracle.com/technetwork/java/javase/downloads/jdk-6u31-download-1501634.html"
   end
 else
   include_recipe "java"
