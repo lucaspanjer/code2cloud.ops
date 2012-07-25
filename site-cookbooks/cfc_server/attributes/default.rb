5e1a4ea89b53bc1357f518a755a8dc906165d2a5
Clint Morgan <clint.morgan@tasktop.com>
2012-07-25 11:07:46 -0700

    4589: Configure internal services which provide URLs to use the org identifier in hostname https://q.tasktop.com/alm/#projects/c2c/task/4589
    Support for wildcard patterns on the no_proxy to hub config

--- site-cookbooks/cfc_server/attributes/default.rb
+++ site-cookbooks/cfc_server/attributes/default.rb
@@ -24,6 +24,8 @@ default[:cfc][:server][:jdbc_port] = "3306"
 default[:cfc][:server][:jdbc_path] = ""
 default[:cfc][:server][:http_proxy] = false
 default[:cfc][:server][:https_proxy] = false
+default[:cfc][:server][:no_proxy_prefix] = false
+  
   
 override[:tomcat][:java_options] = "-Xmx2048M -Xss192K -XX:MaxPermSize=512M -Dlog4j.configuration=file://#{node.cfc.server.opt}/etc/log4j.xml"
 
