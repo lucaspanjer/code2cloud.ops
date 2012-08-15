5914c41945717f8151a4b4e3aae04a23449d4b48
Lucas Panjer <lucas.panjer@tasktop.com>
2012-08-15 14:51:43 -0700

    fix typo

--- solo/solo.rb
+++ solo/solo.rb
@@ -4,5 +4,5 @@ cookbook_path "/opt/code2cloud/chef/cookbooks"
 role_path "/opt/code2cloud/chef/roles"
 
 # Uncomment to use a proxy (eg. for deploy)
-# http_proxy "http:// www-proxy.us.oracle.com:80"
+# http_proxy "http://www-proxy.us.oracle.com:80"
 # https_proxy "http://www-proxy.us.oracle.com:80"
