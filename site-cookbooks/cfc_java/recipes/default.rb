0bf5045df9372244e6a788eda9b645b723376570
Clint Morgan <clint.morgan@tasktop.com>
2012-09-25 17:32:09 -0700

    Remove incorrect comment

--- site-cookbooks/cfc_java/recipes/default.rb
+++ site-cookbooks/cfc_java/recipes/default.rb
@@ -1,5 +1,4 @@
 if platform?("redhat", "oracleserver", "oracle")
-  # FIXME this will not work anymore, need to agree to license agreement
   unless File.exists?("/usr/bin/java")
     raise "Must manually install from: http://www.oracle.com/technetwork/java/javase/downloads/jdk-6u31-download-1501634.html"
   end
