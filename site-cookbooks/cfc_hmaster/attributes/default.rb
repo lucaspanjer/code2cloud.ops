a55d60a0dc642e55751b88976dc5f2c3f5d50ac7
Myles Feichtinger <myles.feichtinger@tasktop.com>
2012-10-01 15:54:54 -0700

    4838: Expose service urls at an additional path
    Task-URL: https://q.tasktop.com/alm/#projects/c2c/task/4838

--- site-cookbooks/cfc_hmaster/attributes/default.rb
+++ site-cookbooks/cfc_hmaster/attributes/default.rb
@@ -2,6 +2,7 @@ include_attribute "cfc_server"
 
 default[:cfc][:service] = "hmaster"
 default[:cfc][:hmaster][:builds_dir] = "#{node.cfc.server.home}/hudson-homes"
-  
+default[:cfc][:hudson][:path] = "/s/"
+    
 override[:tomcat][:java_options] = "-Xmx2048M -Xss192K -XX:MaxPermSize=1024M -Dlog4j.configuration=file://#{node.cfc.server.opt}/etc/log4j.xml"
 
