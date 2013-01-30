89ef00f089aa199020f4ad9938887ea44c4d9099
Clint Morgan <clint.morgan@tasktop.com>
2013-01-29 16:49:25 -0800

    5164: chef scripts should create/update passwords in secure store when appropriate https://q.tasktop.com/alm/#projects/c2c/task/5164 Change-Id: Ia286312e59b239284bb28aaeb36090612d88c5de Reviewed-on: https://q.tasktop.com/alm/s/c2c/gerrit/198 Reviewed-by: Michael Nelson Reviewed-by: Clint Morgan Tested-by: Clint Morgan

--- src/main/chef/cookbooks/c2c_task/attributes/default.rb
+++ src/main/chef/cookbooks/c2c_task/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:c2c][:service] = "task"
 default[:c2c][:db][:task][:username] = "tasks"
 default[:c2c][:db][:wiki][:username] = "wiki"
+default[:c2c][:taskAndWiki][:additional_properties] = []
+
--- src/main/chef/cookbooks/c2c_task/recipes/default.rb
+++ src/main/chef/cookbooks/c2c_task/recipes/default.rb
@@ -3,15 +3,17 @@ node[:c2c][:server][:artifacts] =
 
 include_recipe "c2c_server"
 
-tasksPwd=data_bag_item("secrets", "passwords")["tasks"]
-wikiPwd=data_bag_item("secrets", "passwords")["wiki"]
-
 c2c_log4j_config "tasks"
 c2c_log4j_config "wiki"
+
+tasksPwd=data_bag_item("secrets", "passwords")["tasks"]
+node.set[:c2c][:db][:task][:password] = tasksPwd
+wikiPwd=data_bag_item("secrets", "passwords")["wiki"]
+node.set[:c2c][:db][:wiki][:password] = wikiPwd
+  
   
 template "#{node.c2c.server.opt}/etc/task.properties" do
   source "task.properties.erb"
-  variables :tasksPwd => tasksPwd, :wikiPwd => wikiPwd
   owner node.c2c.user
   group node.tomcat.group
   mode 0660
