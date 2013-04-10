9a5bc8cb3a18b12af83693d0c8b4f7271b659440
Clint Morgan <clint.morgan@tasktop.com>
2013-04-10 13:54:40 -0700

    5112: Liquibase updates should be run on task/wiki before a service is used
    Task-URL: https://q.tasktop.com/alm/#projects/c2c/task/5112

--- src/main/chef/cookbooks/c2c_task/recipes/default.rb
+++ src/main/chef/cookbooks/c2c_task/recipes/default.rb
@@ -20,22 +20,6 @@ template "#{node.c2c.server.opt}/etc/task.properties" do
   notifies :restart, "service[tomcat]"
 end
 
-template "#{node.c2c.server.opt}/bin/liquibase-wiki.rb" do
-  source "liquibase-wiki.rb.erb"
-  variables :wikiPwd => wikiPwd
-  owner node.c2c.user
-  group node.tomcat.group
-  mode 0770
-end
-
-template "#{node.c2c.server.opt}/bin/liquibase-tasks.rb" do
-  source "liquibase-tasks.rb.erb"
-  variables :tasksPwd => tasksPwd
-  owner node.c2c.user
-  group node.tomcat.group
-  mode 0770
-end
-
 c2c_server_deployment "task" do 
   artifacts  [ { "name" => "tasks", "package" => "tasks.web" },
                 { "name" => "wiki", "package" => "wiki.web" } ]
